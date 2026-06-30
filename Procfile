b426f473adfbeeba1b1af22bdda4cef9a6d91d40
Sahla1138 <sahla.sahanas.raihsoft@gmail.com>
2026-06-30 13:02:13 +0530

    Ready for Render

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn myfile.wsgi:application 
--- myfile/settings.py
+++ myfile/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+o%2jxvryt7r&wy)_udlp2ynzg^-yv&hek77i_3+1!1@5jhh9h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost",
+    "127.0.0.1",
+    ".onrender.com",]
 
 
 # Application definition
@@ -121,7 +124,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'static')
 ]
